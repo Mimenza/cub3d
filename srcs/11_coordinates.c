c04a7fbffceca1bd682eba4fd041f6f66af0f0ea
Antton Urtiaga <anurtiag@c3r1s3.42urduliz.com>
2024-04-16 15:26:15 +0200

    player coordinates

---
+++ srcs/11_coordinates.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   11_coordinates.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anurtiag <anurtiag@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/15 13:20:29 by anurtiag          #+#    #+#             */
+/*   Updated: 2024/04/16 13:39:53 by anurtiag         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../incs/cub3d.h"
+
+size_t	ft_mtxlen(const char **s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+void	ft_get_player_dir(t_player **player, char **map, size_t i, size_t j)
+{
+	if (map[i][j] == 'S')
+	{
+		(*player)->rad = 90 * M_PI / 180;
+	}
+	else if (map[i][j] == 'N')
+	{
+		(*player)->rad = 180 * M_PI / 180;
+	}
+	else if (map[i][j] == 'W')
+	{
+		(*player)->rad = 270 * M_PI / 180;
+	}
+	else if (map[i][j] == 'E')
+	{
+		(*player)->rad = 180 * M_PI / 180;
+	}
+}
+
+void	ft_get_player_pos(t_player **player, char **map)
+{
+	size_t	i;
+	size_t	j;
+
+	i = -1;
+	j = -1;
+	while(map[++i])
+	{
+		j = -1;
+		while(map[i][++j])
+		{
+			if (map[i][j] == 'W' || map[i][j] == 'E' || map[i][j] == 'N' || map[i][j] == 'S')
+			{
+				ft_get_player_dir(player, map, i, j);
+				(*player)->pos.x = j + 0.5;
+				(*player)->pos.y = i + 0.5;
+				return ;
+			}
+		}
+	}
+}
+
+void	get_player(t_game *game)
+{
+	t_player	*player;
+
+	player = (t_player *)malloc(sizeof(t_player));
+	if (!player)
+		return ;
+	game->p = player;
+	ft_get_player_pos(&player, game->map.grid);
+	printf("la posicion del jugador es de x: %f, y: %f con una direccion en rad de : %f", player->pos.x, player->pos.y, player->rad);
+	return ;
+}
